cebdd70b3e89b9d7416650abe4e9cc097cf57c94
Suleyma Arriaga <suleyma.arriaga2430@alumnos.udg.mx>
2026-05-22 23:12:14 -0600

    grafica final

--- static/script.js
+++ static/script.js
@@ -4,6 +4,47 @@ const ocupacion = document.getElementById("ocupacion");
 const ventilador = document.getElementById("ventilador");
 const iluminacion = document.getElementById("iluminacion");
 const prediccion = document.getElementById("prediccion");
+const alerta = document.getElementById("alerta");
+
+const ctx = document.getElementById('grafica').getContext('2d');
+
+let temperaturas = [];
+let predicciones = [];
+let etiquetas = [];
+
+const grafica = new Chart(ctx, {
+
+    type: 'line',
+
+    data: {
+
+        labels: etiquetas,
+
+        datasets: [
+
+            {
+                label: 'Temperatura',
+                data: temperaturas,
+                borderWidth: 3
+            },
+
+            {
+                label: 'Predicción IA',
+                data: predicciones,
+                borderWidth: 3
+            }
+
+        ]
+
+    },
+
+    options: {
+
+        responsive: true
+
+    }
+
+});
 
 async function cargarDatos() {
 
@@ -11,29 +52,66 @@ async function cargarDatos() {
 
         const response = await fetch("/datos");
 
-        if (!response.ok) {
-            throw new Error("No se pudieron obtener los datos");
-        }
-
         const data = await response.json();
 
-        console.log("Datos recibidos:", data);
+        temperatura.textContent =
+            data.temperatura + " °C";
 
-        temperatura.textContent = data.temperatura + " °C";
+        luz.textContent =
+            data.luz;
 
-        luz.textContent = data.luz;
+        ocupacion.textContent =
+            data.ocupacion;
 
-        ocupacion.textContent = data.ocupacion;
+        ventilador.textContent =
+            data.ventilador;
 
-        ventilador.textContent = data.ventilador;
+        iluminacion.textContent =
+            data.iluminacion;
 
-        iluminacion.textContent = data.iluminacion;
+        prediccion.textContent =
+            data.prediccion + " °C";
 
-        prediccion.textContent = data.prediccion + " °C";
+        // ALERTA
+
+        if (data.temperatura >= 32) {
+
+            alerta.style.display = "block";
+
+            alerta.innerHTML =
+                "⚠️ Temperatura alta detectada";
+
+        } else {
+
+            alerta.style.display = "none";
+
+        }
+
+        // GRAFICA
+
+        const hora = new Date().toLocaleTimeString();
+
+        etiquetas.push(hora);
+
+        temperaturas.push(data.temperatura);
+
+        predicciones.push(data.prediccion);
+
+        if (etiquetas.length > 10) {
+
+            etiquetas.shift();
+
+            temperaturas.shift();
+
+            predicciones.shift();
+
+        }
+
+        grafica.update();
 
     } catch (error) {
 
-        console.error("Error:", error);
+        console.log(error);
 
     }
 
